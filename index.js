d73f539d924d40116ef46dda0aac6617fa2547d9
Rafaysyed <rafaysyed1@gmail.com>
2024-07-04 01:00:36 +0200

    http requests using node

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.get('/about', (req, res) => {
+    res.send('About page');
+  });
+
+app.get('/contact', (req, res) => {
+  res.send('Contact Page')
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
